d2bf2878cb317a86a009d57c405f7c894a75a575
Vinicius Martins Valim Goncalves <vmgoncalves@band.com.br>
2024-07-29 17:19:26 -0300

    Mudanca postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "d856bd8fb2aa4ac7b3f27a051e15f3c4" #vai garantir a segurança do app chave segura
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
